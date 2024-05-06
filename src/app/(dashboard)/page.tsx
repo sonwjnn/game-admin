7758dabc0a2f66b69999008555654e1c9ff28115
phuc.pham.dev <phuc.pham.dev@gmail.com>
2024-05-06 15:23:07 +0700

    upload dashboard , add checkbox

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -7,10 +7,51 @@ import {
   BreadcrumbList,
   BreadcrumbPage,
   BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb"
+} from '@/components/ui/breadcrumb'
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 
 
-const DashboardPage = async () => {
+
+import { BellIcon, CheckIcon } from '@radix-ui/react-icons'
+
+import { cn } from '@/lib/utils'
+import { Button } from '@/components/ui/button'
+
+
+const notifications = [
+  {
+    title: "Your call has been confirmed.",
+    description: "1 hour ago",
+  },
+  {
+    title: "You have a new message!",
+    description: "1 hour ago",
+  },
+  {
+    title: "Your subscription is expiring soon!",
+    description: "2 hours ago",
+  },
+]
+
+type CardProps = React.ComponentProps<typeof Card>
+
+
+const DashboardPage = async ({ className, ...props }: CardProps) => {
   
   return (
     <div className="flex-col space-y-4 p-8 pt-6">
@@ -30,8 +71,346 @@ const DashboardPage = async () => {
           </BreadcrumbItem>
         </BreadcrumbList>
       </Breadcrumb>
-    
-    
+      
+      
+      <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 m-2 ml-2 mr-2">
+        <Card className={cn("w-[380px]", className) + `m-3 p-2`} {...props}>
+          <CardHeader>
+            <CardTitle>Notifications</CardTitle>
+            <CardDescription>You have 3 unread messages.</CardDescription>
+          </CardHeader>
+          
+          <CardContent className="grid gap-4">
+            <div>
+              {notifications.map((notification, index) => (
+                <div
+                  key={index}
+                  className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0"
+                >
+                  <span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
+                  <div className="space-y-1">
+                    <p className="text-sm font-medium leading-none">
+                      {notification.title}
+                    </p>
+                    <p className="text-sm text-muted-foreground">
+                      {notification.description}
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button className="w-full">
+              <CheckIcon className="mr-2 h-4 w-4" /> Mark all as read
+            </Button>
+          </CardFooter>
+        </Card>
+        <Card className={cn("w-[380px]", className) + `m-3 p-2`} {...props}>
+          <CardHeader>
+            <CardTitle>Notifications</CardTitle>
+            <CardDescription>You have 3 unread messages.</CardDescription>
+          </CardHeader>
+          <CardContent className="grid gap-4">
+            <div>
+              {notifications.map((notification, index) => (
+                <div
+                  key={index}
+                  className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0"
+                >
+                  <span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
+                  <div className="space-y-1">
+                    <p className="text-sm font-medium leading-none">
+                      {notification.title}
+                    </p>
+                    <p className="text-sm text-muted-foreground">
+                      {notification.description}
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button className="w-full">
+              <CheckIcon className="mr-2 h-4 w-4" /> Mark all as read
+            </Button>
+          </CardFooter>
+        </Card>
+        <Card className={cn("w-[380px]", className) + `m-3 p-2`} {...props}>
+          <CardHeader>
+            <CardTitle>Notifications</CardTitle>
+            <CardDescription>You have 3 unread messages.</CardDescription>
+          </CardHeader>
+          <CardContent className="grid gap-4">
+            <div>
+              {notifications.map((notification, index) => (
+                <div
+                  key={index}
+                  className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0"
+                >
+                  <span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
+                  <div className="space-y-1">
+                    <p className="text-sm font-medium leading-none">
+                      {notification.title}
+                    </p>
+                    <p className="text-sm text-muted-foreground">
+                      {notification.description}
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button className="w-full">
+              <CheckIcon className="mr-2 h-4 w-4" /> Mark all as read
+            </Button>
+          </CardFooter>
+        </Card>
+        <Card className={cn("w-[380px]", className) + `m-3 p-2`} {...props}>
+          <CardHeader>
+            <CardTitle>Notifications</CardTitle>
+            <CardDescription>You have 3 unread messages.</CardDescription>
+          </CardHeader>
+          <CardContent className="grid gap-4">
+            <div>
+              {notifications.map((notification, index) => (
+                <div
+                  key={index}
+                  className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0"
+                >
+                  <span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
+                  <div className="space-y-1">
+                    <p className="text-sm font-medium leading-none">
+                      {notification.title}
+                    </p>
+                    <p className="text-sm text-muted-foreground">
+                      {notification.description}
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button className="w-full">
+              <CheckIcon className="mr-2 h-4 w-4" /> Mark all as read
+            </Button>
+          </CardFooter>
+        </Card>
+      </div>
+      
+      <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 m-2 ml-2 mr-2">
+        <Card className={cn("w-[380px]", className) + `m-3 p-2`} {...props}>
+          <CardHeader>
+            <CardTitle>Notifications</CardTitle>
+            <CardDescription>You have 3 unread messages.</CardDescription>
+          </CardHeader>
+          <CardContent className="grid gap-4">
+            <div>
+              {notifications.map((notification, index) => (
+                <div
+                  key={index}
+                  className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0"
+                >
+                  <span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
+                  <div className="space-y-1">
+                    <p className="text-sm font-medium leading-none">
+                      {notification.title}
+                    </p>
+                    <p className="text-sm text-muted-foreground">
+                      {notification.description}
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button className="w-full">
+              <CheckIcon className="mr-2 h-4 w-4" /> Mark all as read
+            </Button>
+          </CardFooter>
+        </Card>
+        <Card className={cn("w-[380px]", className) + `m-3 p-2`} {...props}>
+          <CardHeader>
+            <CardTitle>Notifications</CardTitle>
+            <CardDescription>You have 3 unread messages.</CardDescription>
+          </CardHeader>
+          <CardContent className="grid gap-4">
+            <div>
+              {notifications.map((notification, index) => (
+                <div
+                  key={index}
+                  className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0"
+                >
+                  <span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
+                  <div className="space-y-1">
+                    <p className="text-sm font-medium leading-none">
+                      {notification.title}
+                    </p>
+                    <p className="text-sm text-muted-foreground">
+                      {notification.description}
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button className="w-full">
+              <CheckIcon className="mr-2 h-4 w-4" /> Mark all as read
+            </Button>
+          </CardFooter>
+        </Card>
+        <Card className={cn("w-[380px]", className) + `m-3 p-2`} {...props}>
+          <CardHeader>
+            <CardTitle>Notifications</CardTitle>
+            <CardDescription>You have 3 unread messages.</CardDescription>
+          </CardHeader>
+          <CardContent className="grid gap-4">
+            <div>
+              {notifications.map((notification, index) => (
+                <div
+                  key={index}
+                  className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0"
+                >
+                  <span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
+                  <div className="space-y-1">
+                    <p className="text-sm font-medium leading-none">
+                      {notification.title}
+                    </p>
+                    <p className="text-sm text-muted-foreground">
+                      {notification.description}
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button className="w-full">
+              <CheckIcon className="mr-2 h-4 w-4" /> Mark all as read
+            </Button>
+          </CardFooter>
+        </Card>
+        <Card className={cn("w-[380px]", className) + `m-3 p-2`} {...props}>
+          <CardHeader>
+            <CardTitle>Notifications</CardTitle>
+            <CardDescription>You have 3 unread messages.</CardDescription>
+          </CardHeader>
+          <CardContent className="grid gap-4">
+            <div>
+              {notifications.map((notification, index) => (
+                <div
+                  key={index}
+                  className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0"
+                >
+                  <span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
+                  <div className="space-y-1">
+                    <p className="text-sm font-medium leading-none">
+                      {notification.title}
+                    </p>
+                    <p className="text-sm text-muted-foreground">
+                      {notification.description}
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button className="w-full">
+              <CheckIcon className="mr-2 h-4 w-4" /> Mark all as read
+            </Button>
+          </CardFooter>
+        </Card>
+      </div>
+      
+      <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 m-2 ml-2 mr-2">
+        <Card className={`m-3 p-3 container`}>
+          <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+          </CardHeader>
+          <div className={`w-full`}>
+            <Select>
+              <SelectTrigger className="w-[180px]">
+                <SelectValue placeholder="Theme" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="light">Light</SelectItem>
+                <SelectItem value="dark">Dark</SelectItem>
+                <SelectItem value="system">System</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+          
+          
+        </Card>
+        <Card className={`m-3 p-3 container`}>
+          <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+          </CardHeader>
+          <div className={`w-full`}>
+            <Select>
+              <SelectTrigger className="w-[180px]">
+                <SelectValue placeholder="Theme" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="light">Light</SelectItem>
+                <SelectItem value="dark">Dark</SelectItem>
+                <SelectItem value="system">System</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+        
+        
+        </Card>
+        <Card className={`m-3 p-3 container`}>
+          <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+          </CardHeader>
+          <div className={`w-full`}>
+            <Select>
+              <SelectTrigger className="w-[180px]">
+                <SelectValue placeholder="Theme" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="light">Light</SelectItem>
+                <SelectItem value="dark">Dark</SelectItem>
+                <SelectItem value="system">System</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+        
+        
+        </Card>
+        <Card className={`m-3 p-3 container`}>
+          <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+          </CardHeader>
+          <div className={`w-full`}>
+            <Select>
+              <SelectTrigger className="w-[180px]">
+                <SelectValue placeholder="Theme" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="light">Light</SelectItem>
+                <SelectItem value="dark">Dark</SelectItem>
+                <SelectItem value="system">System</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+        
+        
+        </Card>
+      </div>
+
     
     
     </div>
@@ -39,3 +418,5 @@ const DashboardPage = async () => {
 }
 
 export default DashboardPage
+
+
